36e1a442ce134b68ad94fcb0a959ab763177bc72
vskoushik <vskg2000@gmail.com>
2019-09-28 16:58:57 +0530

    Server code

---
+++ server.py
@@ -0,0 +1,17 @@
+import socket
+
+host= '10.5.1.16'
+port= 5000
+
+server_socket = socket.socket()
+server_socket.bind((host,port))
+server_socket.listen()
+conn, address = server_socket.accept()
+print("Connection from: " + str(address))
+while True:
+    data = conn.recv(1024).decode()
+    print("from connected user: " + str(data))
+    data = input('->')
+    conn.send(data.encode())
+
+conn.close()
